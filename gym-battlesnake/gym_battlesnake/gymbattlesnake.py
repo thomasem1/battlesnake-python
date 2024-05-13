f6b3daa9881b7c69fe5eb9badbb5dc1ad1c4e2cc
Mario Angelic <karlo.angelic@gmail.com>
2024-05-13 20:56:59 +0200

    working version

--- gym-battlesnake/gym_battlesnake/gymbattlesnake.py
+++ gym-battlesnake/gym_battlesnake/gymbattlesnake.py
@@ -41,7 +41,7 @@ class ParallelBattlesnakeEnv(VecEnv):
     def __init__(self, n_threads=4, n_envs=16, n_opponents=7, opponent=None, device=torch.device('cpu'), fixed_orientation=False, use_symmetry=False, dtype=torch.float32):
         # Define action and observation space
         self.action_space = spaces.Discrete(4)
-        self.observation_space = spaces.Box(low=0, high=255, shape=(NUM_LAYERS, LAYER_WIDTH, LAYER_HEIGHT), dtype=np.uint8)
+        self.observation_space = spaces.Box(low=0,high=255, shape=(NUM_LAYERS, LAYER_WIDTH, LAYER_HEIGHT), dtype=np.uint8)
         self.n_opponents = n_opponents
         self.opponent = opponent
         self.n_threads = n_threads
@@ -51,7 +51,7 @@ class ParallelBattlesnakeEnv(VecEnv):
         self.use_symmetry = use_symmetry
         if use_symmetry and not fixed_orientation:
             raise ValueError("symmetry must be used with fixed orientation")
-        self.ptr = env_new(self.n_threads, self.n_envs, self.n_opponents + 1, self.fixed_orientation, self.use_symmetry)
+        self.ptr = env_new(self.n_threads, self.n_envs, self.n_opponents+1, self.fixed_orientation, self.use_symmetry)
         self.dtype = dtype
         super(ParallelBattlesnakeEnv, self).__init__(self.n_envs, self.observation_space, self.action_space)
         self.reset()
@@ -61,24 +61,26 @@ class ParallelBattlesnakeEnv(VecEnv):
 
     def step_async(self, actions):
         # Write player actions into buffer
-        np.copyto(self.getact(0), np.asarray(actions, dtype=np.uint8))
+        np.copyto(self.getact(0), np.asarray(actions,dtype=np.uint8))
         # Get observations for each opponent and predict actions
         all_obs = []
         for i in range(self.n_opponents):
-            all_obs.append(self.getobs(i + 1))
+            all_obs.append(self.getobs(i+1))
         obs = np.vstack(all_obs)
         obs = torch.tensor(obs, dtype=self.dtype).to(self.device)
         with torch.no_grad():
-            acts, _ = self.opponent.predict(obs, deterministic=True)
-        acts = acts.view(self.n_opponents, self.n_envs).cpu().numpy().astype(np.uint8)
+            acts,_ = self.opponent.predict(obs, deterministic=True)
+        acts = acts.view(self.n_opponents, self.n_envs).cpu().detach().numpy().astype(np.uint8)
         for i in range(self.n_opponents):
-            np.copyto(self.getact(i + 1), acts[i].flatten())
+            np.copyto(self.getact(i+1), acts[i].flatten())
+            
         # Step game
         env_step(self.ptr)
 
     def step_wait(self):
+
         info = [{} for _ in range(self.n_envs)]
-        dones = np.asarray([False for _ in range(self.n_envs)])
+        dones = np.asarray([ False for _ in range(self.n_envs) ])
         rews = np.zeros((self.n_envs))
 
         infoptr = env_infoptr(self.ptr)
@@ -100,6 +102,7 @@ class ParallelBattlesnakeEnv(VecEnv):
         env_reset(self.ptr)
         return self.getobs(0)
 
+
     def getobs(self, agent_i):
         obsptr = env_obsptr(self.ptr, agent_i)
         return np.ctypeslib.as_array(obsptr, shape=(self.n_envs, NUM_LAYERS, LAYER_WIDTH, LAYER_HEIGHT))
@@ -114,12 +117,15 @@ class ParallelBattlesnakeEnv(VecEnv):
     def set_attr(self, attr_name, value, indices=None):
         pass
 
-    def env_method(self, method_name, *method_args, indices=None, **method_kwargs):
+    def env_method(self,
+                   method_name,
+                   *method_args,
+                   indices=None,
+                   **method_kwargs):
         pass
 
     def seed(self, seed=None):
         pass
-
 
 class BattlesnakeEnv(VecEnv):
     """Multi-Threaded Multi-Agent Snake Environment"""
@@ -145,19 +151,20 @@ class BattlesnakeEnv(VecEnv):
 
     def step_async(self, actions):
         # Write player actions into buffer
-        np.copyto(self.getact(0), np.asarray(actions, dtype=np.uint8))
+        np.copyto(self.getact(0), np.asarray(actions,dtype=np.uint8))
         # Get observations for each opponent and predict actions
         with torch.no_grad():
-            for i in range(1, self.n_opponents + 1):
+            for i in range(1,self.n_opponents+1):
                 obss = torch.tensor(self.getobs(i), dtype=torch.float32).to(self.device)
-                acts, _ = self.opponents[i - 1].predict(obss, deterministic=True)
-                np.copyto(self.getact(i), acts.cpu().numpy().flatten().astype(np.uint8))
+                acts,_ = self.opponents[i-1].predict(obss, deterministic=True)
+                np.copyto(self.getact(i), acts.detach().cpu().numpy().flatten().astype(np.uint8))
         # Step game
         env_step(self.ptr)
 
     def step_wait(self):
+
         info = [{} for _ in range(self.n_envs)]
-        dones = np.asarray([False for _ in range(self.n_envs)])
+        dones = np.asarray([ False for _ in range(self.n_envs) ])
         rews = np.zeros((self.n_envs))
 
         infoptr = env_infoptr(self.ptr)
@@ -179,6 +186,7 @@ class BattlesnakeEnv(VecEnv):
         env_reset(self.ptr)
         return self.getobs(0)
 
+
     def getobs(self, agent_i):
         obsptr = env_obsptr(self.ptr, agent_i)
         return np.ctypeslib.as_array(obsptr, shape=(self.n_envs, NUM_LAYERS, LAYER_WIDTH, LAYER_HEIGHT))
@@ -193,7 +201,11 @@ class BattlesnakeEnv(VecEnv):
     def set_attr(self, attr_name, value, indices=None):
         pass
 
-    def env_method(self, method_name, *method_args, indices=None, **method_kwargs):
+    def env_method(self,
+                   method_name,
+                   *method_args,
+                   indices=None,
+                   **method_kwargs):
         pass
 
     def seed(self, seed=None):
--- pz-battlesnake/pz_battlesnake/constants.py
+++
@@ -1,6 +0,0 @@
-DEFAULT_COLORS = [
-    "#00FF00",
-    "#0000FF",
-    "#FF00FF",
-    "#FFFF00",
-]
